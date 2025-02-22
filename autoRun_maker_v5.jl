9e356fd74b99280e5b22d759113eddab88e1e182
Woonghee Cho <44767143+MaJangCowBoy@users.noreply.github.com>
2025-02-22 23:39:15 +0900

    version add.

--- autoRun_maker_v5.jl
+++ autoRun_maker_v5.jl
@@ -1,22 +1,42 @@
 using Printf
 
-j2_sweep  = 0.4:0.1:0.8;  jc2_sweep =-0.1:0.1:0.7;
-
-for (i,j2) in enumerate(j2_sweep), (j,jc2) in enumerate(jc2_sweep)
-  id = (i-1) * length(jc2_sweep) + j;
-  fid = open("autoBatch_$(id).sh","w");
-  @printf(fid,"#!/bin/bash\n");
-  @printf(fid,"#SBATCH --cpus-per-task=1\n");
-  @printf(fid,"#SBATCH --job-name=woonghee_CTS\n");
-  @printf(fid,"source ~/.bashrc\n");
-  @printf(fid,"julia_wh master_script_v5.jl %f %f 1> so_%d.txt 2> se_%d.txt\n",j2,jc2,id,id);
-  close(fid);
-end
+j2_sweep  = 0.4:0.1:0.8;  jc2_sweep =-0.1:0.1:0.3;
 
 fid = open("autoRun_v5.sh","w");
+
 @printf(fid,"#!/bin/bash\n")
 for (i,j2) in enumerate(j2_sweep), (j,jc2) in enumerate(jc2_sweep)
+
   id = (i-1) * length(jc2_sweep) + j;
-  @printf(fid,"sbatch autoBatch_%d.sh\n",id);
+  @printf(fid,"julia_wh --threads=3 main_script_v5.jl %f %f 1> so_%d.txt 2> se_%d.txt &\n",j2,jc2,id,id);
+  if id%5 == 0  @printf(fid,"wait\n");  end
 end
-close(fid)
+
+@printf(fid,"wait\n");
+@printf(fid,"exit 0\n");
+close(fid);
+
+#####################################################################################################
+
+# using Printf
+
+# j2_sweep  = 0.4:0.1:0.8;  jc2_sweep =-0.1:0.1:0.7;
+
+# for (i,j2) in enumerate(j2_sweep), (j,jc2) in enumerate(jc2_sweep)
+#   id = (i-1) * length(jc2_sweep) + j;
+#   fid = open("autoBatch_$(id).sh","w");
+#   @printf(fid,"#!/bin/bash\n");
+#   @printf(fid,"#SBATCH --cpus-per-task=1\n");
+#   @printf(fid,"#SBATCH --job-name=woonghee_CTS\n");
+#   @printf(fid,"source ~/.bashrc\n");
+#   @printf(fid,"julia_wh master_script_v5.jl %f %f 1> so_%d.txt 2> se_%d.txt\n",j2,jc2,id,id);
+#   close(fid);
+# end
+
+# fid = open("autoRun_v5.sh","w");
+# @printf(fid,"#!/bin/bash\n")
+# for (i,j2) in enumerate(j2_sweep), (j,jc2) in enumerate(jc2_sweep)
+#   id = (i-1) * length(jc2_sweep) + j;
+#   @printf(fid,"sbatch autoBatch_%d.sh\n",id);
+# end
+# close(fid)
