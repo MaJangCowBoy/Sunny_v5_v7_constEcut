63f76bee16cd6270ba6ccff1d5f1c44f40d4f235
Woonghee Cho <44767143+MaJangCowBoy@users.noreply.github.com>
2025-03-03 23:25:39 +0900

    code improvements.

--- master_script_v7.jl
+++ master_script_v7.jl
@@ -19,13 +19,19 @@ close(f);  #* data stored like, j2 - jc1 - jc2 - Q1 - Q2 - Q3.
 kernel = gaussian(fwhm=0.2);  formfactors = [1 => FormFactor("Co2")];
 cryst = Crystal("CoTaS.cif",symprec=1e-3);  CoTa3S6 = subcrystal(cryst, "Co");
 J1 = 1.311;  Kz = -0.001;
-b1 = [0.00, 0.03, 0.06];  B1 = J1 .* b1;
+b1 = [0.000, 0.003, 0.006, 0.010, 0.020, 0.040, 0.060];  B1 = J1 .* b1;
 #? Basic parameters ?#
 
 #? define q-points for LSWT ?#
 axis1 = [ 1.0, 0.0, 0.0];  N1 = 240;  axis2 = [-0.5, 1.0, 0.0];  N2 = 240;
 qgrid, range1, range2, norm1, norm2 = define_qgrid(cryst,axis1,axis2,N1,N2);
 qpath1, qpath2, range1, range2, norm1, norm2 = define_qline(cryst,axis1,axis2,N1,N2);
+
+axes1 = [ [ 1.0, 0.0, 0.0], [ 0.0, 1.0, 0.0], [-1.0, 1.0, 0.0] ];
+axes2 = [ [-0.5, 1.0, 0.0], [ 1.0,-0.5, 0.0], [ 0.5, 0.5, 0.0] ];
+qgridA, _, _, _, _ = define_qgrid(cryst,axes1[1],axes2[1],N1,N2);
+qgridB, _, _, _, _ = define_qgrid(cryst,axes1[2],axes2[2],N1,N2);
+qgridC, _, _, _, _ = define_qgrid(cryst,axes1[3],axes2[3],N1,N2);
 #? define q-points for LSWT ?#
 
 
@@ -52,8 +58,8 @@ Threads.@threads for i in 1:npar
     if isfile(h5name)  println("File exists: ", h5name);  continue;  end
     #? file existence check ?#
 
-    energies = [0.5 * (J1+J2), 0.6 * (J1+J2), 0.7 * (J1+J2), 0.8 * (J1+J2), 0.9 * (J1+J2)]; # meV
-    regularization = 5e-3;
+    energies = (J1 + J2) * [0.50, 0.55, 0.60, 0.65, 0.7, 0.75, 0.8, 0.85, 0.90]; # meV
+    regularization = 1e-3;
     #? parameter allocation ?#
 
     #? define LSWT part ?#
@@ -76,17 +82,47 @@ Threads.@threads for i in 1:npar
     #? define LSWT part ?#
 
     #? 2D: calculate intensities and store the data ?#
-    data2D_3Q = NaN * zeros(Float64,length(B1),length(energies),N1,N2);  
-    data2D_1Q = NaN * zeros(Float64,length(energies),N1,N2);
+    rawd2D_3Q   = NaN * zeros(Float64,length(B1),length(energies),N1,N2,3);
+    rawd2D_3Q_w = zeros(Float64,length(B1),length(energies),N1,N2,3);
+
+    data2D_3Q   = NaN * zeros(Float64,length(B1),length(energies),N1,N2);
+    data2D_3Q_w = zeros(Float64,length(B1),length(energies),N1,N2);
+
+    data2D_1Q   = NaN * zeros(Float64,length(energies),N1,N2);
+    data2D_1Q_w = zeros(Float64,length(energies),N1,N2);
 
     for (k,swt) in enumerate(swt_3Q)
       try
-        res = intensities(swt, qgrid; energies, kernel);  
-        data2D_3Q[k,:,:,:] = res.data[:,:,:];
-        # for (id,en) in enumerate(energies)
-        #   res = intensities_static(swt, qgrid; bounds = (en-0.1, en+0.1));
-        #   data2D_3Q[k,id,:,:] = res.data[:,:];
-        # end
+        resA = intensities(swt, qgridA; energies, kernel);  rawd2D_3Q[k,:,:,:,1] = resA.data[:,:,:];
+        resB = intensities(swt, qgridB; energies, kernel);  rawd2D_3Q[k,:,:,:,2] = resB.data[:,:,:];
+        resC = intensities(swt, qgridC; energies, kernel);  rawd2D_3Q[k,:,:,:,3] = resC.data[:,:,:];
+        tA = rawd2D_3Q[k,:,:,:,1];  tA1 = reverse(tA,dims=2);  tA2 = reverse(tA1,dims=3);  dA = tA + tA2;
+        tB = rawd2D_3Q[k,:,:,:,2];  tB1 = reverse(tB,dims=2);  tB2 = reverse(tB1,dims=3);  dB = tB + tB2;
+        tC = rawd2D_3Q[k,:,:,:,3];  tC1 = reverse(tC,dims=2);  tC2 = reverse(tC1,dims=3);  dC = tC + tC2;
+        data2D_3Q[k,:,:,:] = dA + dB + dC;
+
+        resA = intensities_bands(swt, qgridA);
+        for (id,e) in enumerate(energies), d in axes(resA.disp,1)
+          Fmat1 = resA.disp[d,:,:] .> e - 0.30;  Fmat2 = resA.disp[d,:,:] .< e + 0.30;
+          filterMatrix = Fmat1 .* Fmat2;
+          rawd2D_3Q_w[k,id,:,:,1] += filterMatrix .* resA.data[d,:,:];
+        end
+        resB = intensities_bands(swt, qgridB);
+        for (id,e) in enumerate(energies), d in axes(resB.disp,1)
+          Fmat1 = resB.disp[d,:,:] .> e - 0.30;  Fmat2 = resB.disp[d,:,:] .< e + 0.30;
+          filterMatrix = Fmat1 .* Fmat2;
+          rawd2D_3Q_w[k,id,:,:,2] += filterMatrix .* resB.data[d,:,:];
+        end
+        resC = intensities_bands(swt, qgridC);
+        for (id,e) in enumerate(energies), d in axes(resC.disp,1)
+          Fmat1 = resC.disp[d,:,:] .> e - 0.30;  Fmat2 = resC.disp[d,:,:] .< e + 0.30;
+          filterMatrix = Fmat1 .* Fmat2;
+          rawd2D_3Q_w[k,id,:,:,3] += filterMatrix .* resC.data[d,:,:];
+        end
+        tA = rawd2D_3Q_w[k,:,:,:,1];  tA1 = reverse(tA,dims=2);  tA2 = reverse(tA1,dims=3);  dA = tA + tA2;
+        tB = rawd2D_3Q_w[k,:,:,:,2];  tB1 = reverse(tB,dims=2);  tB2 = reverse(tB1,dims=3);  dB = tB + tB2;
+        tC = rawd2D_3Q_w[k,:,:,:,3];  tC1 = reverse(tC,dims=2);  tC2 = reverse(tC1,dims=3);  dC = tC + tC2;
+        data2D_3Q_w[k,:,:,:] = dA + dB + dC;
       catch
         println("err in 3Q LSWT for 2D, B1 = ", B1[k], " J2 = ", j2, " Jc2 = ", jc2);
       end
@@ -97,11 +133,15 @@ Threads.@threads for i in 1:npar
         res = intensities(swt, qgrid; energies, kernel);  
         if k == 1  data2D_1Q[:,:,:]  = res.data[:,:,:];
         else       data2D_1Q[:,:,:] += res.data[:,:,:];  end
-        # for (id,en) in enumerate(energies)
-        #   res = intensities_static(swt, qgrid; bounds = (en-0.1, en+0.1));  
-        #   if k == 1  data2D_1Q[id,:,:]  = res.data[:,:];
-        #   else       data2D_1Q[id,:,:] += res.data[:,:];  end
-        # end
+
+        #TODO: Make it please
+        res = intensities_bands(swt, qgrid);
+        for (id,e) in enumerate(energies), d in axes(res.disp,1)
+          FmatA = res.disp[d,:,:] .> e - 0.30;  FmatB = res.disp[d,:,:] .< e + 0.30;
+          filterMatrix = FmatA .* FmatB;
+          data2D_1Q_w[id,:,:] += filterMatrix .* res.data[d,:,:];
+        end
+        #TODO: Make it please
       catch
         println("err in 1Q LSWT for 2D,", " J2 = ", j2, " Jc2 = ", jc2);
       end
@@ -143,9 +183,17 @@ Threads.@threads for i in 1:npar
     write(fid,"j2",j2);  write(fid,"jc1",jc1);  write(fid,"jc2",jc2);
     write(fid,"range1",range1);  write(fid,"norm1",norm1);
     write(fid,"range2",range2);  write(fid,"norm2",norm2);
+    write(fid,"energies",energies);  write(fid,"B1",B1);
     write(fid,"data1Da_3Q", data1Da_3Q);  write(fid,"data1Db_3Q", data1Db_3Q);
     write(fid,"data1Da_1Q", data1Da_1Q);  write(fid,"data1Db_1Q", data1Db_1Q);
+    write(fid,"data2D_3Q", data2D_3Q);  write(fid,"data2D_3Q_w", data2D_3Q_w);  
+    write(fid,"data2D_1Q", data2D_1Q);  write(fid,"data2D_1Q_w", data2D_1Q_w);
     close(fid);
     #? data saving part ?#
   end
 end
+
+###
+# fig = Figure();  ax1 = Axis(fig[1,1]);
+# heatmap!(ax1, data2D_3Q_w[1,3,:,:], colormap = :viridis);
+# save("test.png", fig);
